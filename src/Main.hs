c94904956f050d83ff8b9489b76091fc5063da6f
Solonarv <nstamm@gmx.de>
2018-05-04 20:04:14 +0200

    Factored out component types to a separate module.
    This is mainly to cut down on recompilation times, because World
    is generated by template haskell.

--- src/Main.hs
+++ src/Main.hs
@@ -1,6 +1,4 @@
 {-# language OverloadedStrings #-}
-{-# language TemplateHaskell #-}
-{-# language GeneralizedNewtypeDeriving #-}
 module Main where
 
 import Control.Concurrent (threadDelay)
@@ -19,44 +17,7 @@ import qualified SDL
 
 import Data.Text (Text)
 
-newtype ResAmount = ResAmount { getResAmount :: Double } deriving Show
-instance Component ResAmount where type Storage ResAmount = Map ResAmount
-
-data ResBounds = ResBounds { resBoundsMin :: Double, resBoundsMax :: Double } deriving Show
-instance Component ResBounds where type Storage ResBounds = Map ResBounds
-
-newtype ResRegen = ResRegen { getResRegen :: Double } deriving Show
-instance Component ResRegen where type Storage ResRegen = Map ResRegen
-
-data Castable = Castable { castCost :: Double, castTime :: Double } deriving Show
-instance Component Castable where type Storage Castable = Map Castable
-
-newtype Casting = Casting { castingProgress :: Double } deriving Show
-instance Component Casting where type Storage Casting = Unique Casting
-
-data Damage = Damage { getDamage :: Double } deriving Show
-instance Component Damage where type Storage Damage = Map Damage
-
-newtype Name = Name { getName :: Text } deriving (Show, Eq)
-instance Component Name where type Storage Name = Map Name
-
-newtype DamageDealt = DamageDealt { getDamageDealt :: Sum Double } deriving (Show, Monoid)
-instance Component DamageDealt where type Storage DamageDealt = Global DamageDealt
-
-newtype Time = Time { getTime :: Sum Double } deriving (Show, Monoid)
-instance Component Time where type Storage Time = Global Time
-
-makeWorld "World" [ ''ResAmount
-                  , ''ResBounds
-                  , ''ResRegen
-                  , ''Castable
-                  , ''Casting
-                  , ''Name
-                  , ''Damage
-                  , ''DamageDealt
-                  , ''Time
-                  ]
-type System' = System World
+import World
 
 main :: IO ()
 main = do
