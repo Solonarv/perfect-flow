4522e9bab4cbccead35c3a49a6996c427c656242
Solonarv <nstamm@gmx.de>
2018-06-19 23:29:30 +0200

    Create MonadSystem capability-type class

--- apecs-extra/Apecs/Extra.hs
+++ apecs-extra/Apecs/Extra.hs
@@ -8,5 +8,19 @@ type family HasAll (w :: *) (cs :: [*]) :: Constraint where
   HasAll w '[] = ()
   HasAll w (c ': cs) = (Has w c, HasAll w cs)
 
-getWorld :: System w w
-getWorld = System ask
+class MonadIO m => MonadSystem w m where
+  liftSystem :: System w a -> m a
+  liftSystem act = getWorld >>= runSystem act
+  {-# inline liftSystem #-}
+
+  getWorld :: m w
+  getWorld = liftSystem (System ask)
+  {-# inline getWorld #-}
+
+  {-# MINIMAL liftSystem | getWorld #-}
+
+instance MonadSystem w (System w) where
+  liftSystem = id
+  {-# inline liftSystem #-}
+  getWorld = System ask
+  {-# inline getWorld #-}
